542674957c26739b5d3b812fa424a08a0b107ccd
Darius <partene.darius@gmail.com>
2016-12-10 21:13:25 +0100

    added ads

--- app/src/main/java/com/compilation/mainApp/HolderActivity.java
+++ app/src/main/java/com/compilation/mainApp/HolderActivity.java
@@ -3,15 +3,22 @@ package com.compilation.mainApp;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import com.compilation.R;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
 
 public class HolderActivity extends AppCompatActivity {
 
     /**
      * Activity used for each demo activity.
-     * It gets the demo and sets the toolbar
+     * It gets the demo, sets the toolbar and loads ads
      */
+
+
 
     public void setContentView(int id){
 
@@ -28,6 +35,8 @@ public class HolderActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
+
+        loadAds();
     }
 
     @Override
@@ -38,5 +47,24 @@ public class HolderActivity extends AppCompatActivity {
                 break;
         }
         return true;
+    }
+
+    public void loadAds(){
+        AdView mAdView = new AdView(this);
+        mAdView.setAdSize(AdSize.SMART_BANNER);
+        mAdView.setAdUnitId(getString(R.string.banner_ad_unit_id));
+
+        // Create an ad request.
+        AdRequest.Builder adRequestBuilder = new AdRequest.Builder();
+
+        // Optionally populate the ad request builder.
+        adRequestBuilder.addTestDevice(AdRequest.DEVICE_ID_EMULATOR);
+
+        // Add the AdView to the view hierarchy.
+        RelativeLayout layout = (RelativeLayout) findViewById(R.id.ad_container);
+        layout.addView(mAdView);
+
+        // Start loading the ad.
+        mAdView.loadAd(adRequestBuilder.build());
     }
 }
